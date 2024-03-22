261303d1155b1d691146f83391d7a56bbb51ab80
atognoo_b <hoanganhvu100402@gmail.com>
2024-03-22 21:18:15 +0700

    done

---
+++ HomeTheater/HomeTheater/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace HomeTheater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListStatus = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ListStatus
+            // 
+            this.ListStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ListStatus.FormattingEnabled = true;
+            this.ListStatus.ItemHeight = 31;
+            this.ListStatus.Location = new System.Drawing.Point(43, 566);
+            this.ListStatus.Name = "ListStatus";
+            this.ListStatus.Size = new System.Drawing.Size(976, 345);
+            this.ListStatus.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 491);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Status:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(331, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 44);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "on";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(451, 161);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 44);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "off";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(451, 275);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 44);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "off";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(331, 275);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(86, 44);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "on";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(451, 387);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(86, 44);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "off";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(331, 387);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(86, 44);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "on";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(250, 44);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Watch Movie:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 273);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(238, 44);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Listen to CD:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(35, 385);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(272, 44);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Listen to Radio";
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(569, 161);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(130, 44);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Setting";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(569, 275);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(130, 44);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "Setting";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(569, 387);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(130, 44);
+            this.button9.TabIndex = 13;
+            this.button9.Text = "Setting";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1261, 1003);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ListStatus);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ListStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ HomeTheater/HomeTheater/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeTheater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class Tuner
+    {
+
+    }
+
+    public class StreamingPlayer
+    {
+
+    }
+
+    public class Amplifier
+    {
+        string description;
+        Tuner tuner;
+        StreamingPlayer player;
+      
+
+        public Amplifier(string description)
+        {
+            this.description = description;
+        }
+        public void on()
+        {
+
+        }
+        public void off()
+        {
+
+        }
+        public void setStereoSound()
+        {
+
+        }
+        public void setSurroundSound()
+        {
+
+        }
+        public void setVolume(int level)
+        {
+
+        }
+        public void setTuner(Tuner tuner)
+        {
+            this.tuner = tuner;
+        }
+        public void setStreamingPlayer(StreamingPlayer player)
+        {
+            this.player = player;
+        }
+        public String toString()
+        {
+            return description;
+        }
+    }
+    public class CDPlayer
+    {
+        string description;
+        int currentTrack;
+        Amplifier amplifier;
+        string title;
+        public CDPlayer(string description, Amplifier amplifier)
+        {
+            this.description= description;
+            this.amplifier= amplifier;
+        }
+        public void on()
+        {
+
+        }
+        public void off()
+        {
+
+        }
+        public void eject()
+        {
+
+        }
+
+    }
+
+}
+
